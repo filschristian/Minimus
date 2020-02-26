41b2d7eca1c74ce594d6357d9f7c3ff4849fb391
filschristian <filschristian94@gmail.com>
2020-02-26 11:53:50 +0200

    add followed cities on the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FbService } from '../fb.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  cities = [{
-    name: 'Kigali',
-    country: 'Rwanda'
-  }, {
-    name: 'Rome',
-    country: 'Italy'
-  }]
-  constructor() { }
+  cities = [];
+  sub1: Subscription;
+  constructor(public fb: FbService) { }
 
   ngOnInit(): void {
+    this.sub1 = this.fb.getCities().subscribe((data) => {
+      this.cities = data;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub1.unsubscribe();
   }
 
 }
